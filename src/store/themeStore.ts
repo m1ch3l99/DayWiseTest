569e8139ad0da97f696263ec9a8f8dc838fb12d0
Michel Gerges <mygerges@Alinma.com>
2023-04-15 16:49:34 +0300

    chore: setup zustand and mmkv persist

---
+++ src/store/themeStore.ts
@@ -0,0 +1,30 @@
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+
+import zustandMMKVStorage from './appStorage';
+
+interface IThemeState {
+  isDarkMode: boolean;
+}
+
+interface IThemeStateAction {
+  toggleTheme: () => void;
+}
+
+type IThemeStore = IThemeState & IThemeStateAction;
+
+const useThemeStore = create<IThemeStore>()(
+  persist(
+    set => ({
+      isDarkMode: false,
+      toggleTheme: (): void =>
+        set(state => ({ isDarkMode: !state.isDarkMode })),
+    }),
+    {
+      name: 'theme',
+      storage: createJSONStorage(() => zustandMMKVStorage),
+    },
+  ),
+);
+
+export default useThemeStore;
