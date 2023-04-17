bb5e95200807a9a6f285c58072ea0d63bffcec23
Michel Gerges <mygerges@Alinma.com>
2023-04-17 15:37:43 +0300

    chore: added eslintignore file

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable @typescript-eslint/no-var-requires */
-
 const { getDefaultConfig } = require('metro-config');
 
 module.exports = (async () => {
