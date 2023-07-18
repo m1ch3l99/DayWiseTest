a4bc18945b6a519add34c0d0194049700230d67a
Michel Gerges <mygerges@Alinma.com>
2023-07-18 14:45:34 +0300

    chore: setup react query provider

--- App.tsx
+++ App.tsx
@@ -3,11 +3,16 @@ import 'i18n/i18n';
 import MainNavigator from 'navigation/MainNavigator';
 import { StyleSheet } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 const App = (): JSX.Element => (
-  <GestureHandlerRootView style={styles.rootViewStyle}>
-    <MainNavigator />
-  </GestureHandlerRootView>
+  <QueryClientProvider client={queryClient}>
+    <GestureHandlerRootView style={styles.rootViewStyle}>
+      <MainNavigator />
+    </GestureHandlerRootView>
+  </QueryClientProvider>
 );
 
 export default App;
